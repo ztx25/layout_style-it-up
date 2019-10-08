502f4c8558beb2430bab71af9e554fbbce48d854
mgrinko <glukym@gmail.com>
2019-10-08 14:32:03 +0300

    Simple dev

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -1,20 +1,18 @@
 'use strict';
-
 // https://github.com/garris/BackstopJS#advanced-scenarios
 
 const backstop = require('@mate-academy/backstop-config');
-
 const { basicScenario } = backstop;
 
 const basic = {
   ...basicScenario,
   label: 'Elementary test',
-  referenceUrl:
-    'https://mate-academy.github.io/layout_solutions/style-it-up/',
+  referenceUrl: basicScenario.referenceUrl + '/style-it-up/',
 };
 
 const config = {
   ...backstop,
+  fileNameTemplate: '{scenarioLabel}',
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
   viewports: [
@@ -27,17 +25,17 @@ const config = {
   scenarios: [
     {
       ...basic,
-      label: 'entire-document',
+      label: 'Entire document',
       selectors: ['document'],
     },
     {
       ...basic,
-      label: 'default-block',
+      label: 'Block',
       selectors: ['#qa-block'],
     },
     {
       ...basic,
-      label: 'hovered-block',
+      label: 'Hovered block',
       hoverSelectors: ['#qa-block'],
       selectors: ['#qa-block'],
     },
